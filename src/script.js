55c2a10248d41a72517a24f0ba4b4542c9e9ec84
Zarha <zarhaselene@gmail.com>
2024-12-17 11:14:18 +0100

    Modal

--- src/script.js
+++ src/script.js
@@ -10,3 +10,32 @@ function toggleMenu() {
 
 menuBtn.addEventListener("click", toggleMenu);
 menuCloseBtn.addEventListener("click", toggleMenu);
+
+// Modal Logic
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const modal = document.getElementById("accessibleModal");
+
+let lastFocusedElement;
+
+function openModal() {
+  lastFocusedElement = document.activeElement;
+  modal.classList.remove("hidden");
+  modal.setAttribute("aria-hidden", "false");
+  closeModalBtn.focus();
+  document.addEventListener("keydown", handleModalKeyDown);
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  modal.setAttribute("aria-hidden", "true");
+  lastFocusedElement.focus(); // Return focus
+  document.removeEventListener("keydown", handleModalKeyDown);
+}
+
+function handleModalKeyDown(event) {
+  if (event.key === "Escape") closeModal();
+}
+
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
